8fa36b7dc693eadb8fad50f508d1f1f177776fb6
Aaron Beers <aaron.beers@gmail.com>
2014-06-04 23:48:48 -0400

    What

--- hello.c
+++ hello.c
@@ -2,7 +2,11 @@
 
 int main()
 {
+
+  int four = 4;
   printf("Hello, World!");
   printf("\n");
+  printf("The number four is: %d\n", four);
+  printf("Four plus one is: %d\n", four+1);
   return 0;
 }
